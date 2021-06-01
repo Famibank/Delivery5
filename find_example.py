c33c9e67caf131a60944b35739a74261aa0ee49b
yonashanel <61009752+yonashanel@users.noreply.github.com>
2021-06-01 15:38:59 +0200

    Update find_example.py

--- find_example.py
+++ find_example.py
@@ -1,3 +1,4 @@
+import collections
 import pymongo
 
 client = pymongo.MongoClient('mongodb+srv://admin:admin@cluster0.diqaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE')
@@ -5,11 +6,24 @@ client = pymongo.MongoClient('mongodb+srv://admin:admin@cluster0.diqaz.mongodb.n
 db = client['test-database'] #a database
 collection = db['test-collection']
 
+# we check if the customer is in our collection
+
+CPR_input = input('Write down the CPR: ')
+
+find_query = { "CPR" : (CPR_input) }
+mydoc = collection.find(find_query)
+
+for x in mydoc:
+    print(x)
+
+
+
+'''
+# prints first one
 x = collection.find_one()
 print(x)
 
 
-'''
 # print all
 
 for x in collection.find():
