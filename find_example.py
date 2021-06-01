a0a9e4ec402fdfc81718e129e2d6bec6610138cf
parispip <85165625+parispip@users.noreply.github.com>
2021-06-01 14:52:06 +0200

    Update find_example.py

--- find_example.py
+++ find_example.py
@@ -1,6 +1,6 @@
 import pymongo
 
-client = pymongo.MongoClient('mongodb+srv://tom:parole123@banking-app-python.zfwkg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE')
+client = pymongo.MongoClient('mongodb+srv://admin:admin@cluster0.diqaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE')
 
 db = client['test-database'] #a database
 collection = db['test-collection']
