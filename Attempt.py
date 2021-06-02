345df135a6fb3157979eaf7bb49529b2c2b02dbc
cori-fl <85174902+cori-fl@users.noreply.github.com>
2021-06-02 12:58:52 +0200

    Update Attempt.py

--- Attempt.py
+++ Attempt.py
@@ -2,6 +2,7 @@
 from pymongo import MongoClient
 import random
 import pyinputplus as pyip
+from getpass import getpass
 
 client = MongoClient(
     'mongodb+srv://admin:admin@cluster0.diqaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE')
@@ -13,7 +14,7 @@ collection = db['test-collection']  # like a folder for more documents
 class Account:
     def register(self):
         cpr = pyip.inputNum("CPR to register\n", min=999999999, max=9999999999)
-        password_db = pyip.inputStr(prompt="Password?\n", blank=True, blockRegexes='password') # fix password, only allows int
+        password_db = getpass(prompt="Password?\n") # fix password, only allows int
         full_name = pyip.inputStr(prompt="Full Name?\n", blank=True)
         bank_account_number = random.randint(1000000, 99999999)  # need to make so user gets assigned one after registering
         user = {"CPR": cpr,
@@ -23,12 +24,12 @@ class Account:
         collection.insert_one(user)
 
     def login(self):
-        login_name = input('What is your login CPR?')
+        login_name = input('What is your login CPR?\n')
         user = collection.find_one({'CPR': int(login_name)})#connect to CPR collection in mongo, already found user, no need to test if CPR is correct
         if not user:
             print('User not found')
             return False
-        user_password = input('What is your login password?')
+        user_password = getpass('What is your login password?\n')
 
         if user["password"] == user_password:
             return True
