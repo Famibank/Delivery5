34f6c090eba52a083b1a6df066c89607ef975ed3
yonashanel <61009752+yonashanel@users.noreply.github.com>
2021-06-02 11:44:38 +0200

    Add files via upload

---
+++ Attempt.py
@@ -0,0 +1,35 @@
+# should run on any other computer than Mac
+from pymongo import MongoClient
+import random
+import pyinputplus as pyip
+
+client = MongoClient(
+    'mongodb+srv://admin:admin@cluster0.diqaz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&ssl_cert_reqs=CERT_NONE')
+# was missing SSL certificate for y computer, that's why the addition at the end of the MngoDB connection URI
+
+db = client['test-database']  # a database
+collection = db['test-collection']  # like a folder for more documents
+
+class Account:
+    def register(self):
+        cpr = pyip.inputNum("CPR to register\n", min=999999999, max=9999999999)
+        password_db = pyip.inputStr(prompt="Password?\n", blank=True, blockRegexes='password') # fix password, only allows int
+        full_name = pyip.inputStr(prompt="Full Name?\n", blank=True)
+        bank_account_number = random.randint(1000000, 99999999)  # need to make so user gets assigned one after registering
+        user = {"CPR": cpr,
+                "password": password_db,
+                "full_name": full_name,
+                "bank_account_number": bank_account_number} # can add more things like email, gender. phone nr, remember to dd variables asking for them
+        collection.insert_one(user)
+
+    def login(self):
+        login_name = input('What is your login CPR?')
+        user = collection.find_one({'CPR': int(login_name)})#connect to CPR collection in mongo, already found user, no need to test if CPR is correct
+        if not user:
+            print('User not found')
+            return False
+        user_password = input('What is your login password?')
+
+        if user["password"] == user_password:
+            return True
+        return False
