1d1347ae59d395d32a86c0ce1563e2e2e75e5e7a
yonashanel <61009752+yonashanel@users.noreply.github.com>
2021-06-02 22:14:13 +0200

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -6,19 +6,18 @@ import datetime as dt
 import requests
 
 def show_graph():
-    # We ask user for the stock symbol and the time frame.
+    # We ask the user for the stock symbol and the dataframe he/she is interested in.
     company = input('Put a stock symbol (ex. AAPL, FB, TSLA):  ')
 
-
-    date1 = input('Enter the start date in YYYY-MM-DD format: ')
+    date1 = str(input('Enter the start date in YYYY-MM-DD format: '))
     year, month, day = map(int, date1.split('-'))
     start = dt.datetime(year, month, day)
 
-    date2 = input('Enter the end date in YYYY-MM-DD format: ')
+    date2 = str(input('Enter the end date in YYYY-MM-DD format: '))
     year, month, day = map(int, date2.split('-'))
     end = dt.datetime(year, month, day)
 
-    data = web.DataReader(company, 'yahoo', start, end)
+    data = web.DataReader(company, 'yahoo', start, end) # We download the data from Yahoo Finance API
 
     plt.plot(data['Close']) #Starts the plot with the closing price of each day for the selected time period.
     plt.ylabel(f'{company} Stock price ($)') #Names the y axis according to which stock was chosen.
