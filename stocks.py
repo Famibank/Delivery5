211d17c6a43ab5bcf98c59663930a23b9154528b
yonashanel <61009752+yonashanel@users.noreply.github.com>
2021-06-02 12:14:05 +0200

    Update stocks.py

--- stocks.py
+++ stocks.py
@@ -9,7 +9,7 @@ import requests
 def show_graph():
     # We ask user for the stock ticker and the time frame
     company = input('Put a stock symbol (ex. AAPL, FB, TSLA):  ')
-    # start = dt.datetime(input("Put start day-month-year: "))
+    
 
     date1 = input('Enter the start date in YYYY-MM-DD format: ')
     year, month, day = map(int, date1.split('-'))
@@ -22,6 +22,8 @@ def show_graph():
     data = web.DataReader(company, 'yahoo', start, end)
     
     plt.plot(data['Close'])
-    plt.ylabel('f{company} stock price ($)') # fix the name
+    # plt.figure(figsize=(12, 6))
+    plt.ylabel(f'{company} Stock price ($)')
+    plt.xlabel('Date (YYYY,MM,DD)')
+    plt.xticks(rotation=70, ha='right')
     plt.show()
-
